b3b8fde7f4ab018759ca0deb072eff4c598664c6
marushnic16101984 <marushnic@ukr.net>
2022-10-04 19:27:20 +0300

    add 390

--- app/js/main.js
+++ app/js/main.js
@@ -11,13 +11,13 @@ $(function () {
 
 
   $(document).on('click', '.js-menu_toggle.burger', function (e) {
-    e.preventDefault(); $('.menu__list, .menu__item').stop();
+    e.preventDefault(); $('.menu__list, .menu__item, .footer__adress, .footer__data, .header__tablet').stop();
 
     $('.side_menu').css({ 'left': '0px' });
 
-    var count = $('.menu__item').length;
-    $('.menu__list').slideDown((count * .0) * 0);
-    $('.menu__item').each(function (i) {
+    var count = $('.menu__item, .footer__data, .header__tablet').length;
+    $('.menu__list, .footer__adress, .header__tablet ').slideDown((count * .0) * 0);
+    $('.menu__item, .footer__data, .header__tablet ').each(function (i) {
       var thisLI = $(this);
       timeOut = 0 * i;
       setTimeout(function () {
@@ -30,16 +30,17 @@ $(function () {
   });
 
   $(document).on('click', '.js-menu_toggle.opened', function (e) {
-    e.preventDefault(); $('.menu__list, .menu__item').stop();
+    e.preventDefault(); $('.menu__list, .menu__item, .footer__adress, .footer__data, .header__tablet').stop();
 
     $('.side_menu').css({ 'left': '-800px' });
 
-    var count = $('.menu__item').length;
-    $('.menu__item').css({
+    var count = $('.menu__item, .footer__data, .header__tablet').length;
+    $('.menu__item, .footer__data, .header__tablet').css({
+      'transition': '1s',
       'opacity': '0',
       'margin-left': '-20px'
     });
-    $('.menu__list').slideUp(0);
+    $('.menu__list, .footer__adress, .header__tablet').slideUp(0);
   });
 
   $('.js-menu_toggle.burger').on('click', function () {
